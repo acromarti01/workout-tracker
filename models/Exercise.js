e1805da4cc01778dbfed2b744481c77031f46abf
Anthony Cromartie <acromarti@gmail.com>
2021-09-24 23:59:35 -0500

    modified schema in Exercise.js

--- models/Exercise.js
+++ models/Exercise.js
@@ -1,18 +1,39 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// {
+//     type: 'resistance',
+//     name: 'Lateral Pull',
+//     duration: 20,
+//     weight: 300,
+//     reps: 10,
+//     sets: 4,
+// },
+
 const exerciseSchema = new Schema({
-  name: {
-    // type: String,
-    // trim: true,
-    // required: "Enter a name for transaction"
-  },
-  value: {
-    
-  },
-  date: {
-    
-  }
+    type: {
+        type: String,
+        trim: true,
+    },
+    name: {
+        type: String,
+        trim: true,
+    },
+    duration: {
+        type: Number,
+    },
+    distance: {
+        type: Number,
+    },
+    weight: {
+        type: Number,
+    },
+    reps: {
+        type: Number,
+    },
+    sets: {
+        type: Number,
+    },
 });
 
 const Exercise = mongoose.model("Exercise", exerciseSchema);
